f947de2dbc50de1619ca18830d051098d753f13f
joshawesome12 <joshawesome12@yahoo.com>
2017-11-01 19:49:48 -0500

    Fills in all imgUrls in database

--- server/schema.sql
+++ server/schema.sql
@@ -13,25 +13,26 @@ CREATE TABLE items (
   health int(255),
   damage int(255),
   agility int(3),
+  imgUrl varchar(100),
   PRIMARY KEY (ID)
 );
 
-INSERT INTO items (faction,name, health, damage, agility) values ("Knights","Warden", 130, 28.50, 2),
- ("Knights","Conqueror", 140, 25.33, 1),
- ("Knights","Peacekeeper", 120, 21.74, 3),
- ("Knights","Lawbringer", 150, 25, 1),
- ("Knights","Centurion", 110, 20.76, 2),
- ("Knights","Gladiator", 120, 20, 3),
- ("Vikings","Raider", 140, 24.06, 2),
- ("Vikings","Warlord", 140, 19, 1),
- ("Vikings","Berserker", 120, 21.13, 1),
- ("Vikings","Valkyrie", 120, 21.38, 1),
- ("Vikings","Highlander", 125, 23, 1),
- ("Samurai","Kensei", 125, 23.64, 2),
- ("Samurai","Shugoki", 160, 34.55, 1),
- ("Samurai","Orochi",120, 23.75, 3),
- ("Samurai","Nobushi", 120, 23, 1),
- ("Samurai","Shinobi", 110, 21.17, 3);
+INSERT INTO items (faction,name, health, damage, agility, imgUrl) values ("Knights","Warden", 130, 28.50, 2, 'http://tinyurl.com/y9n2j6wc'),
+ ("Knights","Conqueror", 140, 25.33, 1, 'http://tinyurl.com/ybkml6da'),
+ ("Knights","Peacekeeper", 120, 21.74, 3, 'http://tinyurl.com/yb6vyq3p'),
+ ("Knights","Lawbringer", 150, 25, 1, 'http://tinyurl.com/y8fzbspu'),
+ ("Knights","Centurion", 110, 20.76, 2, 'http://tinyurl.com/ybr5ka9l'),
+ ("Knights","Gladiator", 120, 20, 3, 'http://tinyurl.com/ybsa6cme'),
+ ("Vikings","Raider", 140, 24.06, 2, 'http://tinyurl.com/y8o3olg7'),
+ ("Vikings","Warlord", 140, 19, 1, 'http://tinyurl.com/y8y86oxo'),
+ ("Vikings","Berserker", 120, 21.13, 1, 'http://tinyurl.com/y7a4ec9f'),
+ ("Vikings","Valkyrie", 120, 21.38, 1, 'http://tinyurl.com/y9mgzt27'),
+ ("Vikings","Highlander", 125, 23, 1, 'http://tinyurl.com/ydfbt5ge'),
+ ("Samurai","Kensei", 125, 23.64, 2, 'http://tinyurl.com/y9q6jffd'),
+ ("Samurai","Shugoki", 160, 34.55, 1, 'http://tinyurl.com/ybapg8u3'),
+ ("Samurai","Orochi",120, 23.75, 3, 'http://tinyurl.com/yb4f8qof'),
+ ("Samurai","Nobushi", 120, 23, 1, 'http://tinyurl.com/ybunpds5'),
+ ("Samurai","Shinobi", 110, 21.17, 3, 'http://tinyurl.com/ydaa72m7');
 
 
 
