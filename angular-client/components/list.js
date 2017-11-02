6102e62117704c06adc2d356711fe42281279e93
joshawesome12 <joshawesome12@yahoo.com>
2017-11-02 15:09:02 -0500

    Play sounds and shake heros

--- angular-client/components/list.js
+++ angular-client/components/list.js
@@ -3,6 +3,25 @@ angular.module('app')
   bindings: {
     items: '<',
   },
-  controller: function() {},
+  controller: function() {
+    this.slash = function(){
+      document.getElementById('').play();
+    };
+    this.playsound = function(){
+      document.getElementById('fightsound').play();
+      console.log('Sound should be playing');
+    };
+    this.fight = function(){
+      this.playsound();
+      var f1 = document.getElementById('fighter1');
+      var f2 = document.getElementById('fighter2');
+      f1.classList.add("shake")
+      f2.classList.add("shake")
+      setTimeout(function(){
+        f1.classList.remove("shake")
+        f2.classList.remove("shake")
+      },3000)
+    }
+  },
   templateUrl: '/templates/list.html'
 });
